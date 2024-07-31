c30c05605ba5bf16dc81792a0449ac8b85f4cf7a
Lucas Su <lijie.su93@gmail.com>
2024-07-31 21:25:34 +0930

    delete ';'

--- src/app/(auth)/sign-up/[[...sign-up]]/page.jsx
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.jsx
@@ -3,7 +3,7 @@ import { SignUp } from "@clerk/nextjs";
 export default function Page() {
   return (
     <div className="flex justify-center align-middle p-20">
-      <SignUp />;
+      <SignUp />
     </div>
   );
 }
